2b591ab6f470695cc830a65d8e950edd42023ac6
BoKyu Jung <70844171+JBK501@users.noreply.github.com>
2024-02-19 20:01:58 +0900

    Observer_part2

--- src/active_menu.js
+++ src/active_menu.js
@@ -5,8 +5,6 @@
 // 보여지는 섹션
 // - 다수의 섹션이 동시에 보여진다면, 가장 첫번째 섹션을 선택한다.
 // - 마지막 contact섹션이 보여진다면, 가장 마지막 섹션을 선택한다.
-// 1. 보여지는 섹션에대한 정보를 가져온다.
-//
 
 // 아이디를 기준으로 섹션과 메뉴아이템을 가져온다.
 const selectionIds = [
@@ -23,8 +21,12 @@ const navItems = selectionIds.map((id) =>
   document.querySelector(`[href="${id}"]`)
 );
 const visibleSections = selectionIds.map(() => false); // 섹션아이디 요소와 길이랑 일치해야함. 그래서 매핑함.
+let activeNavItem = navItems[0];
 
-const options = {};
+const options = {
+  rootMargin: "-20% 0px 0px 0px",
+  threshold: [0, 0.98],
+};
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach((section) => observer.observe(section));
 
@@ -34,26 +36,38 @@ function observerCallback(entries) {
     const index = selectionIds.indexOf(`#${entry.target.id}`); //보여지는 섹션의 인덱스 정보를 가져온다.
     visibleSections[index] = entry.isIntersecting; // 현재 요소가 보여지는지 여부를 저장한다.
 
+    // 마지막 섹션인지 여부를 구한다.
     selectLastOne =
       index === selectionIds.length - 1 &&
       entry.isIntersecting &&
-      entry.intersectionRatio >= 0.99;
+      entry.intersectionRatio >= 0.95;
 
-    console.log(entry.target.id);
-    console.log(entry.isIntersecting);
+    // console.log(entry.target.id);
+    // console.log(entry.isIntersecting);
   });
 
-  console.log(visibleSections);
-  console.log("무조건 마지막 섹션!", selectLastOne);
+  // console.log(visibleSections);
+  // console.log("무조건 마지막 섹션!", selectLastOne);
 
+  // 활성화 시킬 메뉴아이템의 위치를 구한다.
   const navIndex = selectLastOne
     ? selectionIds.length - 1
     : findIntersecting(visibleSections);
+  // console.log("타깃 인덱스 아이디", sections[navIndex]);
 
-  console.log("타깃 인덱스 아이디", sections[navIndex]);
+  // 현재 메뉴를 활성화 한다.
+  SelectNavItem(navIndex);
 }
 
 function findIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function SelectNavItem(navIndex) {
+  const navItem = navItems[navIndex];
+  if (!navItem) return;
+  activeNavItem.classList.remove("active");
+  activeNavItem = navItem;
+  activeNavItem.classList.add("active");
 }
